5116897809456a8659d2c7def11d0b81860d7b46
HMD <hamed_ranjbar@live.com>
2021-08-31 19:29:32 +0430

    err fixed

--- app-server/controllers/index.js
+++ app-server/controllers/index.js
@@ -4,7 +4,7 @@ const apiOptions = {
     server: 'http://localhost:3000'
 };
 if (process.env.NODE_ENV === 'production') {
-    apiOptions.server = 'https://pure-temple-67771.herokuapp.com';
+    apiOptions.server = 'https://ttc-moghadam.herokuapp.com';
 }
 
 const get_home = (req, res) => {
